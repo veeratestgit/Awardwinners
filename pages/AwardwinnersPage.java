eb34026ca20e8d20cd0a26cf0dc3aaa09fb19348
Veerabhadra gouda <144895676+veeratestgit@users.noreply.github.com>
2023-12-13 14:34:31 +0530

    Update AwardwinnersPage.java

--- pages/AwardwinnersPage.java
+++ pages/AwardwinnersPage.java
@@ -5,49 +5,64 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class Award_winnersPage extends TestBase {
-		
-	@FindBy(xpath="//div[@id='listpromoproduct']/div[1]/div/a/img")
-	WebElement book;
+public class AwardWinnersPage extends TestBase {
 	
-	@FindBy(xpath="//div[@id='ctl00_phBody_ProductDetail_divAddtoCart']/input")
-	WebElement buy;
+	@FindBy(xpath="//select[@id='ddlSort']")
+	WebElement sort;
+	
+	@FindBy(xpath="//select[@id='ddlSort']/option[4]")
+	WebElement Discount;
+	
+	@FindBy(xpath="//div[@id='listpromoproduct']/descendant::a")
+	WebElement Book;
+	
+	@FindBy(xpath="//div[@id='ctl00_phBody_ProductDetail_divaction']/descendant::input[1]")
+	WebElement Cart;
 	
 	@FindBy(xpath="//span[@class='itemcount']/label[@id='ctl00_lblTotalCartItems']")
-	WebElement cart;
+	WebElement cart1;
 	
 	@FindBy(xpath="//input[@value='Buy']")
 	WebElement buy1;
 	
-	@FindBy(xpath="//input[@id='ctl00_phBody_SignIn_txtEmail']")
-	WebElement email_box;
+	@FindBy(xpath="//input[@id='ctl00_cpBody_ShoppingCart_lvCart_savecontinue']")
+	WebElement save;
 	
-	@FindBy(xpath="//input[@id='ctl00_phBody_SignIn_txtPassword']")
-	WebElement password_box;
-	
-	@FindBy(xpath="//a[@id='ctl00_phBody_SignIn_btnLogin']")
-	WebElement button;
-	public void selectbook() throws InterruptedException
-	{
-		Thread.sleep(3000);
-		book.click();
-		buy.click();
-	}
-	public void buybook() throws InterruptedException {
-		Thread.sleep(1500);
-		cart.click();
-		buy1.click();
+	@FindBy(xpath=" //input[@id='ctl00_cpBody_lvCustomerAdd_ctrl0_btnUseAddress']")
+	WebElement Address;
 		
-	}
-	public void LoginDetails(String email,String password) throws InterruptedException {
-		Thread.sleep(5000);
-		email_box.sendKeys(email);
-		password_box.sendKeys(password);
-		button.click();
 	
-	}
-	public Award_winnersPage() {
+	public AwardWinnersPage(WebDriver driver) {
 		PageFactory.initElements(driver, this);	
 		
+	}
+	
+	public void sortaward() {
+		sort.click();
+		Discount.click();
+	}
+	public void buybook() throws InterruptedException {
+		Thread.sleep(1000);
+		Book.click();
+		Cart.click();	
+	}
+	public void cart() throws InterruptedException{
+		Thread.sleep(1000);
+		Cart1.click();
+		buy1.click();
+	}
+	public void save() throws InterruptedException {
+			Thread.sleep(1500);
+			Address.click();
+			Thread.sleep(1500);
+			save.click();	
+	}
+	public boolean logo() { 
+		    WebElement logo = driver.findElement(By.xpath("//img[@id="ctl00_imglogo"]"));
+		    return logo.isDisplayed();
+	}
+	public boolean logoutvisible() { 
+		    WebElement logoutvisible = driver.findElement(By.xpath("//a[@id='ctl00_lnkbtnLogout']"));
+		    return logoutvisible.isDisplayed();
 	}
 }
